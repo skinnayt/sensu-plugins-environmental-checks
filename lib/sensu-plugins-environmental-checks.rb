dd8e45f164a0034d4fed5acc9f8d877b58b77ed1
Matt Jones <mattjones@yieldbot.com>
2015-06-03 10:48:18 -0400

    deploy

--- lib/sensu-plugins-environmental-checks.rb
+++ lib/sensu-plugins-environmental-checks.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-environmental-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsEnvironmantalChecks
-  class << self
-  end
-
-  class << self
-  end
-end
